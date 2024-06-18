daa4081847a5aa4db36966672ca26c0bf94f0ffc
binhan321 <binhanvio@gmail.com>
2024-06-18 16:57:45 +0700

    cm2

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <MenuTest></MenuTest>
+    <MenuTest />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import MenuTest from './components/MenuTest'
 
 export default {
   name: 'App',
+  data(){
+    return{
+      
+    }
+  },
   components: {
-    HelloWorld,
     MenuTest
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/components/MenuTest.vue
+++ src/components/MenuTest.vue
@@ -1,12 +1,34 @@
 <template>
     <div>
         <h5>MENU</h5>
+        <input type="text" v-model="name">
+        <br>
+        Name:{{ name }}
+        <br>
+        <input type="checkbox" v-model="check"> đồng ý
+        <br>
+        <div>
+            <input type="radio" value="1" v-model="checkRadio"> 1
+            <br>
+            <input type="radio" value="2" v-model="checkRadio"> 2
+            <br>
+            <input type="radio" value="3" v-model="checkRadio"> 3
+            <br>
+            Value: {{ checkRadio }}
+        </div>
     </div>
 </template>
 
 <script>
-export default{
-    
+export default {
+    name:'MenuTest',
+    data(){
+        return {
+            name: "Phí Văn Lò",
+            check: true,
+            checkRadio: 1
+        }
+    },
 }
 </script>
 <script scoped>
